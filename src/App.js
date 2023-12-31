d9707a33d91819327c275a63c54e0bae7b9297a6
wsigma21 <wallstlbl@gmail.com>
2023-12-31 16:30:29 +0900

    feat: 前へ・次へボタンを追加

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -15,7 +17,9 @@ function App() {
       let res = await getAllPokemon(initialURL);
       // 各ポケモンの詳細なデータを取得
       loadPokemon(res.results);
-      // console.log(res.results);
+      setNextURL(res.next);
+      setPrevURL(res.previous);
+      console.log(res);
       setLoading(false);
     }
     fetchPokemonData();
@@ -33,7 +37,30 @@ function App() {
     setPokemonData(_pokemonData);
   }
 
-  console.log(pokemonData);
+  // console.log(pokemonData);
+
+  const handlePrevPage =  async () => {
+    if (!prevURL) return;
+    console.log("handlePrevPage")
+    setLoading(true);
+    let res = await getAllPokemon(prevURL);
+    console.log(res);
+    await loadPokemon(res.results);
+    setNextURL(res.next);
+    setPrevURL(res.previous);
+    setLoading(false);
+  };
+
+  const handleNextPage = async () => {
+    setLoading(true);
+    
+    let res = await getAllPokemon(nextURL);
+    console.log(res);
+    await loadPokemon(res.results);
+    setNextURL(res.next);
+    setPrevURL(res.previous);
+    setLoading(false);
+  };
 
   return (
     <>
@@ -48,6 +75,10 @@ function App() {
             })}
           </div>  
         }
+        <div className="btn">
+          <button onClick={handlePrevPage}>前へ</button>
+          <button onClick={handleNextPage}>次へ</button>
+        </div>
       </div>
     </>
   );
