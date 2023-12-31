8c947e8bf6fdcebe0c0c4d4a422651ee95d85fbd
wsigma21 <wallstlbl@gmail.com>
2023-12-31 14:48:39 +0900

    add: APIからデータ取得

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useState, useEffect } from "react";
+import { getAllPokemon, getPokemon } from "./utils/pokemon"
 import './App.css';
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得する
+      let res = await getAllPokemon(initialURL);
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      // console.log(res.results);
+      setLoading(false);
+    }
+    fetchPokemonData();
+  },[]);
+
+  const loadPokemon = async (data) => {
+    // _pokemonDataにreturnされたpokmeonRecordが入る
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    )
+    setPokemonData(_pokemonData);
+  }
+  
   return (
     <div className="App">
-
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ):
+        <h1>ポケモンデータを取得しました</h1>
+      }
     </div>
   );
 }
---
+++ src/utils/pokemon.js
@@ -0,0 +1,16 @@
+export const getAllPokemon = (url) => {
+  // fetchのPromiseチェーンの処理が終わるまで待つ約束
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  });
+};
+
+export const getPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  })
+}
